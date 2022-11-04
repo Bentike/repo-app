053f0f4dd0e07e33c5baf29518f21aa4243c2a3b
Bentike <ajadiyusuf73@gmail.com>
2022-11-04 08:45:14 +0100

    formated github data

--- src/Component/UserCard.js
+++ src/Component/UserCard.js
@@ -5,11 +5,11 @@ const UserCard = (props) => {
    const {url, full_name, name, id, default_branch} = props.data;
     return(
         <div className="card">
-            <p className='username'>{full_name}</p>
-            <p className="cardContent">{name}</p>
-            <p className="cardContent">{url}</p>
-            <p className="cardContent">{default_branch}</p>
-            <p className="cardContent">{id}</p>
+            <p className='username'>Name: {full_name}</p>
+            <p className="cardContent">AppName: {name}</p>
+            <p className="cardContent">AppUrl: {url}</p>
+            <p className="cardContent">Branch: {default_branch}</p>
+            <p className="cardContent">Id: {id}</p>
         </div>
     );
 }
