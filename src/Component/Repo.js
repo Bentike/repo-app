8ffb63cc757f30b981fc4b8cfacaf442c3bde2cf
Bentike <ajadiyusuf73@gmail.com>
2022-11-05 14:36:59 +0100

    populated Repo.js with repository data

--- src/Component/Repo.js
+++ src/Component/Repo.js
@@ -1,19 +1,41 @@
-import {useState, useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import './Styles/Repo.css';
 
-const Repo = ({name}) => {
+const Repo = () => {
     
-    const [repo, setRepo] = useState([]);
+ // gets repoName from local storage
+   const getRepoName = () => {
+    return JSON.parse(localStorage.getItem('repoName'));
+  }
 
+  const [repo, setRepo] = useState([]);
+  const [owner, setOwner] = useState([]);
+    
     useEffect(() => {
-        fetch("https://api.github.com/repos/Bentike/${name}")
+        fetch(`https://api.github.com/repos/Bentike/${getRepoName()}`)
              .then(res => res.json())
-             .then(data => console.log(data));
-    })
+             .then(data => {
+                setRepo(data);
+                setOwner(data.owner);
+            });
+    }, []);
+
+    const {created_at, default_branch, fullname, language, name, visibility} = repo;
+    const {avatar_url, login, id, url} = owner;
     
     return(
             <div className='repo'>
                 <h1>Repo Details To be displayed Here</h1>
+                <p>{created_at}</p>
+                <p>{default_branch}</p>
+                <p>{fullname}</p>
+                <p>{language}</p>
+                <p>{name}</p>
+                <p>{visibility}</p>
+                <p>{login}</p>
+                <p>{id}</p>
+                <p>{url}</p>
+                <img src={avatar_url} alt='owner'/>
             </div>
     )
 }
