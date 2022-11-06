50e6a20508e61747d18bfef3d276bf9ef95ab3d0
Bentike <ajadiyusuf73@gmail.com>
2022-11-06 14:39:20 +0100

    added button to test error boundary

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
 import './Styles/Home.css';
 
 const Home = () => {
+   const [error, setError] = useState(false);
+
+   if(error){
+     throw Error("Something Went Wrong!!");
+   }
+
+   const makeError = () => {
+      setError(true);
+   }
+
     return(
          <>
            <Helmet>
@@ -23,6 +34,7 @@ const Home = () => {
                      to view my github repositories.
                  </p>
              </div>
+             <button onClick={makeError}>Test ErrorBoundary</button>
              <Outlet/>
          </>
     )
