78919c9d1203687344f5924fec6685dd0a1d8de9
Bentike <ajadiyusuf73@gmail.com>
2022-11-06 15:49:14 +0100

    added link in NotFound page to navigate back home

--- src/Component/NotFound.js
+++ src/Component/NotFound.js
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
+
 const style = {
     textAlign: "center"
  }
  
  const NotFound = () => {
      return(
-         <>
-           <h1 style={style}>Page Not Found</h1>
-         </>
+         <div style={style}>
+           <h1>Page Not Found</h1>
+           <Link to='/'>Home</Link>
+         </div>
      )
  }
  
